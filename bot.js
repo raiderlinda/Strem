335170a0a7ea91e81a9a3bfcdb1b8c7273d684b9
raiderlinda <49811957+raiderlinda@users.noreply.github.com>
2019-04-20 19:38:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "R";
 client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+client.user.setGame(`!~ Raider ,,,`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
 });
 
 const developers = ["562313255322779657"]
