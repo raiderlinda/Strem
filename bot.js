685d2c30e0bce80dfb98fd3b2dd8f8303bfec2e0
raiderlinda <49811957+raiderlinda@users.noreply.github.com>
2019-04-20 15:32:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,45 @@
+const Discord = require('discord.js');
+const { Client, Util } = require('discord.js');
+const client = new Discord.Client();
+const { PREFIX, GOOGLE_API_KEY } = require('./config');
+const prefix = '1'
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube(GOOGLE_API_KEY);
+const queue = new Map();
+const moment = require('moment');
+const fs = require("fs");
+const coolDown = new Set();
+const Canvas = require('canvas');
+const db = require('quick.db')
+const ms = require('ms')
+const bot = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
 const developers = ["562313255322779657"]
 const adminprefix = "R";
 client.on('message', message => {
