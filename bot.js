58d8d1945365ad9f258c722558779a54b9c2b9aa
raiderlinda <49811957+raiderlinda@users.noreply.github.com>
2019-04-20 15:29:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,3 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "R";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!~ Raider ,,,`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
-});
-
 const developers = ["562313255322779657"]
 const adminprefix = "R";
 client.on('message', message => {
